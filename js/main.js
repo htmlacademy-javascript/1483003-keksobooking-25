cec33ed0d5219f974ca5e8e225c710c23e079828
Eremeev_Alexander <sanya.eremey@mail.ru>
2023-03-18 19:27:28 +0300

    refactoring main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,2 @@
-/* import { similarOffers } from './generate-offers.js';
-
-console.log(similarOffers); */
-
+// import { similarOffers } from './generate-offers.js';
+// console.log(similarOffers);
